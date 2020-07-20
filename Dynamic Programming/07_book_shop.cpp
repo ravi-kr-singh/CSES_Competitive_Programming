a9a9c16d9ae34f5e45a5e3a4118c706d7fea054a
Ravi Kumar Singh <imrk016@gmail.com>
2020-07-20 19:27:26 +0530

    Update 07_book_shop.cpp

--- Dynamic Programming/07_book_shop.cpp
+++ Dynamic Programming/07_book_shop.cpp
@@ -34,10 +34,7 @@ Explanation: You can buy books 1 and 3. Their price is 4+5=9 and the number of p
 */ 
 
 #include<iostream>
-#include<vector>
 using namespace std;
-
-
 
 int main(){
 
@@ -56,13 +53,13 @@ int main(){
 
     for(int i=0;i<=n;i++){
         for(int j=0;j<=x;j++){
-            if(i==0 || j == 0)
+            if(j == 0 || i == 0)
                 dp[i][j] = 0;
             else if(price[i-1] <= j)
                 dp[i][j] = max(pages[i-1]+dp[i-1][j-price[i-1]],dp[i-1][j]);
+            
             else
                 dp[i][j] = dp[i-1][j];
-            
         }
     }
 
