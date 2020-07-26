b09b67d4edec907a8464f48016e1d8daad85ca68
Ravi Kumar Singh <imrk016@gmail.com>
2020-07-26 21:09:13 +0530

    Create 04_finding_periods.cpp

---
+++ String Algorithms/04_finding_periods.cpp
@@ -0,0 +1,24 @@
+/*
+Time limit: 1.00 s Memory limit: 512 MB
+A period of a string is a prefix that can be used to generate the whole string by repeating the prefix. The last repetition may be partial. For example, the periods of abcabca are abc, abcabc and abcabca.
+
+Your task is to find all period lengths of a string.
+
+Input
+
+The only input line has a string of length n consisting of characters a–z.
+
+Output
+
+Print all period lengths in increasing order.
+
+Constraints
+1≤n≤10^6
+Example
+
+Input:
+abcabca
+
+Output:
+3 6 7
+*/
