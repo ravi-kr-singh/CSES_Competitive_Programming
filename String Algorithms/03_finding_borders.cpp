00497fffd41316a2ed400f95d20f45315e623174
Ravi Kumar Singh <imrk016@gmail.com>
2020-07-26 21:09:00 +0530

    Create 03_finding_borders.cpp

---
+++ String Algorithms/03_finding_borders.cpp
@@ -0,0 +1,38 @@
+/*
+Time limit: 1.00 s Memory limit: 512 MB
+A border of a string is a prefix that is also a suffix of the string but not the whole string. For example, the borders of abcababcab are ab and abcab.
+
+Your task is to find all border lengths of a given string.
+
+Input
+
+The only input line has a string of length n consisting of characters a–z.
+
+Output
+
+Print all border lengths of the string in increasing order.
+
+Constraints
+1≤n≤10^6
+Example
+
+Input:
+abcababcab
+
+Output:
+2 5
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    string str;
+    cin>>str;
+    int n = str.size();
+
+    
+
+
+    return 0;
+}
